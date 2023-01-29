466f24bc8ad9a2e744bb1fabd4521a70a3c4b4ad
oanadragan <oanadragan17@gmail.com>
2023-01-29 14:53:01 +0200

    Final Project

---
+++ src/test/java/Pages/LoginModel.java
@@ -0,0 +1,61 @@
+package Pages;
+
+import javax.xml.bind.annotation.XmlElement;
+
+public class LoginModel{
+
+    private AccountModel account;
+    private String userError;
+    private String passwordError;
+
+    public LoginModel() {
+    }
+
+    public LoginModel(String username, String password, String userError, String passwordError) {
+        AccountModel ac = new AccountModel();
+        ac.setUsername(username);
+        ac.setPassword(password);
+
+        this.account = ac;
+        this.userError = userError;
+        this.passwordError = passwordError;
+    }
+
+    public AccountModel getAccount() {
+        return account;
+    }
+
+    @XmlElement
+    public void setAccount(AccountModel account) {
+        this.account = account;
+    }
+
+    public String getUserError() {
+        return userError;
+    }
+
+    @XmlElement
+    public void setUserError(String userError) {
+        this.userError = userError;
+    }
+
+    public String getPasswordError() {
+        return passwordError;
+    }
+
+    @XmlElement
+    public void setPasswordError(String passwordError) {
+        this.passwordError = passwordError;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "LoginData{" +
+                "account={username:" + account.getUsername() + ",password:" + account.getPassword() +
+                "}, userError='" + userError + '\'' +
+                ", passwordError='" + passwordError + '\'' +
+                '}';
+    }
+}
---
+++ src/test/java/Tests/LoginDataTest.java
@@ -0,0 +1,59 @@
+package Tests;
+
+import Pages.LoginModel;
+import Pages.LoginPage;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class LoginDataTest extends BaseTest{
+
+    @DataProvider(name = "jsonDp")
+    public Iterator<Object[]> jsonDpCollection() throws IOException {
+        Collection<Object[]> dp = new ArrayList<>();
+//      here is starting deserialization of json into LoginModel object
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        File f = new File("src\\test\\resources\\data\\loginData.json");
+        LoginModel[] lms = objectMapper.readValue(f, LoginModel[].class);
+
+        for (LoginModel lm : lms)
+            dp.add(new Object[]{lm});
+
+        return dp.iterator();
+    }
+
+    @Test(dataProvider = "jsonDp")
+    public void loginWithJsonTest(LoginModel lm) {
+        System.out.println(lm);
+        LoginPage loginPage = new LoginPage(driver);
+
+//       open login page
+        System.out.println("Open Login page");
+        driver.get(baseUrl + "/autentificare");
+
+//         login
+        loginPage.login(lm.getAccount().getUsername(), lm.getAccount().getPassword());
+        System.out.println("Login button was pressed");
+
+        String expectedUsernameError = lm.getUserError();
+        String expectedPasswordErr = lm.getPasswordError();
+
+
+        System.out.println("Verify expected errors present:\n expected username Error:" + expectedUsernameError);
+        Assert.assertTrue(loginPage.checkErr(expectedUsernameError, "userError"));
+
+        System.out.println("Expected Password Error:" + expectedPasswordErr);
+        Assert.assertTrue(loginPage.checkErr(expectedPasswordErr, "passwordError"));
+    }
+
+
+
+}
